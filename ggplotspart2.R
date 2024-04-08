dcab26826a53a268499aaefe2c9406f4528846f8
Elliott S <17tmsmith@gmail.com>
2024-04-08 11:04:13 -0400

    updated ggplot 04/08

--- ggplotspart2.R
+++ ggplotspart2.R
@@ -11,70 +11,62 @@ gss22a <- gss22 %>%
   drop_na(mntlhlth)
 
 gss22a$sex <- as.factor(gss22a$sex)
-gss22a$mntlhlth <- as.numeric
-
-ggplot(
-  data = gss22a,
-  mapping = aes(x = hrs1, y = mntlhlth, na.rm = T)) +
-  geom_point(mapping = aes(color = sex, shape = sex)) +
-  geom_smooth(method = "lm", se = F, color = "black") +
-  labs(
-    title = "Poor mental health predicted by hours worked.",
-    subtitle = "Depicted by differing gender plots.",
-    x = "Hours worked", y = "Mental Health",
-    color = "Sex", shape = "Sex")
-gss22a$race <- as.factor(gss22a$race)  
-
-ggplot(
-  data = gss22a,
-  mapping = aes(x = hrs1, y = mntlhlth, na.rm = T)) +
-  geom_point(mapping = aes(color = race, shape = race)) +
-  geom_smooth(method = "lm", se = F, color = "black") +
-  facet_wrap(~sex + race)
-  labs(
-    title = "Poor mental health predicted by hours worked.",
-    subtitle = "Depicted by differing gender plots.",
-    x = "Hours worked", y = "Mental Health",
-    color = "Sex", shape = "Sex")
-
-gss22a$race <- as.factor(gss22a$race)  
- 
-ggplot(
-  data = gss22a,
-  mapping = aes(x = hrs1, y = mntlhlth, na.rm = T)) +
-  geom_point(mapping = aes(color = sex, shape = sex)) +
-  geom_smooth(method = "lm", se = F, color = "black") +
-  labs(
-    title = "Poor mental health predicted by hours worked.",
-    subtitle = "Depicted by gender.",
-    x = "Hours worked", y = "Mental Health",
-    color = "Sex", shape = "Sex") 
-
-gss22a$age <- as.numeric(gss22a$age)
-gss22a$hrs1 <- as.numeric(gss22a$hrs1)
-ggplot(
-  data = gss22a,
-  mapping = aes(x = mntlhlth, y = age, na.rm = T)) +
-  geom_point(mapping = aes(color = hrs1)) +
-  geom_smooth(method = "lm", se = F, color = "red")
-
-gss22a$wrkstat <- as.factor(gss22a$wrkstat)
-ggplot(gss22a, aes(x = wrkstat, y = mntlhlth)) +
-  geom_boxplot()
-
-ggplot(gss22a, aes(x = mntlhlth, color = wrkstat, fill = wrkstat)) +
-  geom_density(alpha = 0.5)
-
-ggplot(gss22a, aes(x = mntlhlth, color = wrkstat, fill = wrkstat)) +
-  geom_density(alpha = 0.5) +
-facet_wrap(~wrkstat )
-
-ggplot(gss22a, aes(x = hrs1, y = mntlhlth)) +
-  geom_point(aes(color = sex)) +
-  facet_wrap(~race) +
-  geom_smooth(method = "lm", se = F)
-
 gss22a$depress <- as.factor(gss22a$depress)
-ggplot(gss22a, aes(x = fct_infreq(wrkstat), fill = depress))+
+gss22a$wrkstat <- as.factor(gss22a$wrkstat)
+
+##bargraph
+ggplot(gss22a, aes(x = fct_infreq(wrkstat, fill = sex)))+
   geom_bar() +
-  facet_grid(~race)
+  labs(
+    title = "Work Status",
+    subtitle = "Subsetted with Depression",
+    x = "Work Status (1 = Full Time, 2 = Part Time, 3 = Laid Off)", y = "Count")
+
+##histogram
+ggplot(gss22a, aes(x = mntlhlth))+
+  geom_histogram() +
+  labs(
+    title = "Histogram of Poor Mental Health Days"
+  )
+
+##histogram subsetted by workstatus
+
+ggplot(gss22a, aes(x = mntlhlth, fill = wrkstat))+
+  geom_histogram() +
+  facet_wrap(~wrkstat) +
+  labs(
+    title = "Histogram of Poor Mental Health Days",
+    subtitle = "Faceted by Work Status")
+
+## violin plot
+ggplot(gss22a, aes(x = wrkstat, y = mntlhlth, fill = wrkstat))+
+  geom_violin() +
+  labs(
+    title = "Work Status",
+  subtitle = "Violin Plots of Work Status and Mental Health",
+  x = "Work Status (1 = Full Time, 2 = Part Time, 3 = Laid Off)", y = "Count of Poor Mental Health Days")
+
+##regression/scatterplots
+ggplot(
+  data = gss22a,
+  mapping = aes(x = hrs1, y = mntlhlth)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = F, color = "red") +
+  labs(
+    title = "Scatter Plot of Poor Mental health Predicted by Hours Worked.",
+    subtitle = "Depicting no association between variables.",
+    x = "Hours worked", y = "Mental Health")
+
+##multi of dv and iv by sex
+ggplot(
+  data = gss22a,
+  mapping = aes(x = hrs1, y = mntlhlth)) +
+  geom_point(mapping = aes(color = sex, shape = sex)) +
+  geom_smooth(method = "lm", se = F, color = "black") +
+  facet_wrap(~sex) +
+  labs(
+    title = "Scatter Plot of Poor Mental health Predicted by Hours Worked..",
+    subtitle = "Depicted by differing gender plots.",
+    x = "Hours worked", y = "Mental Health",
+    color = "Sex", shape = "Sex")
+
